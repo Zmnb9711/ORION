b59f52ff27b16b8fac9c2ab251537e5e104a8394
Zmnb9711 <belashov.alexey@gmail.com>
2026-08-08 14:39:03 +0300

    Register TTS audio API

--- orion/app.py
+++ orion/app.py
@@ -76,6 +76,7 @@ for _router_module in (
     "orion.dialogue_runtime_api",
     "orion.voice_core_api",
     "orion.speech_scheduler_api",
+    "orion.tts_audio_api",
     "orion.launch_api",
     "orion.dcs_installations_api",
     "orion.dcs_readiness_api",
