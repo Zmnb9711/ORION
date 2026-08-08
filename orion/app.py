c3ffd267a055f1714279bb9f69f4ddcada0d52a3
Zmnb9711 <belashov.alexey@gmail.com>
2026-08-08 16:30:14 +0300

    Register recovery DCS launch API

--- orion/app.py
+++ orion/app.py
@@ -101,6 +101,7 @@ for _router_module in (
     "orion.onboarding_runtime_api",
     "orion.startup_health_api",
     "orion.recovery_orchestrator_api",
+    "orion.recovery_launch_api",
     "orion.dcs_process_api",
     "orion.flight_console_api",
     "orion.flight_readiness_api",
