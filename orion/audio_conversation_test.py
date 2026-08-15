d84052a1527cbf3bb713df0f93881c5acf9c9fce
Zmnb9711 <belashov.alexey@gmail.com>
2026-08-15 13:17:30 +0300

    Route conversational audio test through faster-whisper

--- orion/audio_conversation_test.py
+++ orion/audio_conversation_test.py
@@ -8,10 +8,10 @@ from uuid import uuid4
 from pydantic import BaseModel
 
 from orion.audio_device_config import audio_device_config
+from orion.faster_whisper_stt import recognize_wav
 from orion.native_wasapi_player import NativeWasapiPlayer
 from orion.tts_audio import AudioRenderRequest, TtsBackend, VoiceProfile
 from orion.voice_core import VoiceAgent
-from orion.whisper_cpp_stt import recognize_wav
 from orion.windows_sapi_backend import WindowsSapiBackend
 from orion.windows_wasapi_backend import WasapiDirection, WasapiEndpoint
 
