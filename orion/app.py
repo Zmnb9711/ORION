31a910a5392c4bf2018a5f72fd5d43d475fd915b
Zmnb9711 <belashov.alexey@gmail.com>
2026-08-16 13:17:56 +0300

    voice: register text bridge API

--- orion/app.py
+++ orion/app.py
@@ -83,6 +83,7 @@ for _router_module in (
     "orion.jtac_api",
     "orion.dialogue_runtime_api",
     "orion.voice_core_api",
+    "orion.voice_text_bridge_api",
     "orion.speech_scheduler_api",
     "orion.tts_audio_api",
     "orion.windows_audio_worker_api",
